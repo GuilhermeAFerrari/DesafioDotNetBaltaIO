ee175589c4df56dcf9d897016210009087fee48c
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-17 20:14:09 -0300

    add testes

---
+++ src/DesafioDotNetBaltaIO.API/IApiAssemblyMarker.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioDotNetBaltaIO.Application.Interfaces
+{
+    public interface IApiAssemblyMarker
+    {
+        
+    }
+}
