5410e0818dbeeb27e2cefbad369c73c46e28fad1
Guilherme Ferrari <guile.ferrari@hotmail.com>
2023-10-18 20:37:51 -0300

    fix: solve swagger UI to production

--- src/DesafioDotNetBaltaIO.API/Program.cs
+++ src/DesafioDotNetBaltaIO.API/Program.cs
@@ -111,14 +111,14 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de IBGE v1");
     });
-}
+//}
 
 app.UseHttpsRedirection();
 
